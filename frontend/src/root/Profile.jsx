f232859c91471c67224253bed562e4aeca228087
Jackson Yao <91328227+yaojiandashuaige@users.noreply.github.com>
2023-10-13 01:00:53 +1100

    new one

--- frontend/src/root/Profile.jsx
+++ frontend/src/root/Profile.jsx
@@ -1,56 +1,64 @@
-import React from 'react';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import NavigationBtn from '../components/NavigationBtn';
 import { FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox } from '@mui/material';
-import { apiCall, checkEmail, checkName, checkSkills, checkWorkRight, extractUId } from '../components/HelpFunctions';
+import { apiCall, checkEmail, checkName, checkSkills, checkWorkRight, fileToDataUrl } from '../components/HelpFunctions';
 
 // const ProfileDiv = styled('div')({
 //   padding: '10px'
 // })
 
-function Profile() {
+const Profile = () => {
   const param = useParams();
   const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [workRight, setWorkRight] = React.useState('');
   const [skill, setSkill] = React.useState('');
+  const [avatarUrl, setAvatarUrl] = React.useState(null);
 
-  React.useEffect(() => {
-    apiCall(`/profile/details/${param.userId}`, 'GET', {})
-    .then(( data ) => {
-      if (data.error) {
-        alert(data.error)
-      } else {
-        console.log(data)
-        console.log(data.token)
-      }
-    })
+  useEffect(() => {
+    if (localStorage.getItem('userId')) {
+      const res = apiCall(`/getUserInfo/${localStorage.getItem('userId')}`, 'GET')
+      res.then((data) => {
+        if (data.error) {
+          alert(data.error)
+        } else {
+          setEmail(data.email)
+          setName(data.name)
+        }
+      })
+    }
   });
-  
-  async function updateProfile1() {
-    const res = apiCall(`/updateprofile/f1/${param.userId}/`, 'PUT', { 'name': name, 'email': email });
-    res.then((data) => {
-      if (data.error) {
-        alert(data.error)
-      } else {
-        console.log(data)
-      }
-    })
+
+  async function updateProfile() {
+    if (checkEmail(email) && checkName(name) && checkWorkRight(workRight) && checkSkills(skill)) {
+      const res = apiCall(`/profile/update/${param.userId}/`, 'PUT',
+        { 'name': name, 'email': email, 'workRight': workRight, 'skill': skill, "avatarUrl": avatarUrl });
+      res.then((data) => {
+        if (data.error) {
+          alert(data.error)
+        } else {
+          console.log(data)
+        }
+      })
+    }
   }
 
-  async function updateProfile2() {
-    const res = apiCall(`/updateprofile/f2/${param.userId}/`, 'POST', { 'workright': workRight, 'skill': skill });
-    res.then((data) => {
+  useEffect(() => {
+    apiCall(`/profile/details/${param.userId}/`, 'GET', {}).then((data) => {
       if (data.error) {
         alert(data.error);
       } else {
-        console.log(data)
+        setEmail(data.email);
+        setName(data.name);
+        setWorkRight(data.workRight);
+        setSkill(data.skill)
       }
-    })
-  }
+    });
+  }, [])
 
   const handleCheckboxChange = (event) => {
     const value = event.target.value;
@@ -61,17 +69,13 @@ function Profile() {
     }
   };
 
+  const handleReplaceAvatar = () => {
+    setAvatarUrl(null);
+  };
 
   function keyProfile(e) {
     if (e.key === 'Enter') {
-      checkProfile();
-    }
-  }
-
-  function checkProfile() {
-    if (checkEmail(email) && checkName(name) && checkWorkRight(workRight) && checkSkills(skill)) {
-      updateProfile1();
-      updateProfile2();
+      updateProfile();
     }
   }
 
@@ -81,7 +85,7 @@ function Profile() {
       <Box
         component="form"
         sx={{
-          py: 10,
+          py: 8,
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
@@ -89,6 +93,27 @@ function Profile() {
         noValidate
         autoComplete="off"
       >
+        <FormControl style={{ textAlign: 'center', position: 'relative' }}>
+          <div style={{ width: '130px', height: '130px', border: '2px solid #3489db', borderRadius: '50%', overflow: 'hidden' }}>
+            <img src={avatarUrl || 'empty-avatar.png'} alt="Avatar" style={{ width: '100%', height: '100%', objectFit: 'cover', objectPosition: 'center center' }} />
+          </div>
+          {!avatarUrl && (
+            <div>
+              <label htmlFor="upload" style={{ backgroundColor: '#3498db', padding: '5px 10px', borderRadius: '5px', marginBottom: '10px' }}>
+                Upload
+              </label>
+              <input type="file" id="upload" accept="image/jpeg, image/png, image/jpg"
+                onChange={async (e) => { setAvatarUrl(await fileToDataUrl(e.target.files[0])); }} 
+                style={{ display: 'none' }} />
+            </div>
+          )}
+          {avatarUrl && (
+            <Button onClick={handleReplaceAvatar} style={{ left: '50%', transform: 'translateX(-50%)', backgroundColor: 'transparent', color: '#3489db' }}>
+              Replace
+            </Button>
+          )}
+        </FormControl>
+        <br></br>
         <FormControl style={{ width: "400px" }}>
           <FormLabel style={{ fontWeight: 'bold', color: 'black' }}>Email:</FormLabel>
           <TextField id="filled-basic" label="email" variant="filled" value={email} style={{ width: '400px' }}
@@ -168,7 +193,7 @@ function Profile() {
               setSkill(e.target.value);
             }} />
         </FormControl>
-        <Button id='registerbutton' role="profile" variant="contained" color="success" onKeyDown={keyProfile} onClick={checkProfile} sx={{ marginTop: '30px' }}>Save</Button>
+        <Button id='registerbutton' role="profile" variant="contained" color="success" onKeyDown={keyProfile} onClick={updateProfile} sx={{ marginTop: '30px' }}>Save</Button>
       </Box >
     </>
   )
