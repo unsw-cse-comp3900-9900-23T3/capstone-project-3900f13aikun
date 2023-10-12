07d3053bcbf3e4b14479e84100608ee5c1d113dd
Jackson Yao <91328227+yaojiandashuaige@users.noreply.github.com>
2023-10-12 16:52:55 +1100

    new

--- frontend/src/root/SignIn.jsx
+++ frontend/src/root/SignIn.jsx
@@ -8,18 +8,13 @@ import { styled } from '@mui/system';
 import { Typography } from '@mui/material';
 import NavigationBtn from '../components/NavigationBtn';
 
-const Logindiv = styled('div')({
-  padding: '10px'
-})
-
 // signin page
-function SignIn () {
+function SignIn() {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const navigate = useNavigate();
-  // localStorage.clear()
   // login function
-  async function login () {
+  async function login() {
     const res = apiCall('/login', 'POST', { 'email': email, 'password': password });
     res.then((data) => {
       if (data.error) {
@@ -27,41 +22,56 @@ function SignIn () {
       } else {
         console.log(data)
         localStorage.setItem('token', data.token);
-        navigate('/dashboard')
+        localStorage.setItem('userId', data.user.id);
+
+        if (data.user.role === 'Industry partner') {
+          navigate('/dashboard/industryp')
+        } else if (data.user.role === 'Student') {
+          navigate('/dashboard/student')
+        } else if (data.user.role === 'Academic supervisor') {
+          navigate('/dashboard/academics')
+        }
       }
     })
   }
 
- 
+
   return (
-        <>
-          <NavigationBtn></NavigationBtn>
-          
+    <>
+      <NavigationBtn></NavigationBtn>
+
+      <div style={{ background: '#E2E6E6', width: '1540px', height: '100%' }}>
+        <div  style={{ width: '550px', height: '370px', background: 'white', margin: 'auto', marginTop: '50px' }}>
+          <div style={{ position: 'relative', top: '10px', left: '220px', marginBottom: '30px', fontSize: '30px' }}>login</div>
           <Box
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        
-        <TextField id="filled-basic" label="Email" variant="filled" style={{width:'400px'}} 
-          onChange={(e) => {
-            setEmail(e.target.value);
-        }}/>
-        <br></br>
-        <TextField id="filled-basic" label="password" variant="filled" style={{width:'400px'}} 
-          onChange={(e) => {
-            setPassword(e.target.value);
-        }}/>
-        <br></br>
-        <Typography>
-          <Link to="/forgot">Forgot password?</Link>
-        </Typography>
-      </Box>
-          <Button id='loginbutton' role='login' variant="contained" color="success" onClick={login} sx={{ marginTop: '30px' }}>login</Button>
-        </>
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+
+            <TextField id="filled-basic" label="Email" variant="filled" style={{ width: '400px', marginLeft:'60px' }}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }} />
+            <br></br>
+            <TextField id="filled-basic" label="password" variant="filled" style={{ width: '400px', marginLeft:'60px'  }}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }} />
+            <br></br>
+            <Typography>
+              <Link to="/forgot" style={{ marginLeft:'60px'}}>Forgot password?</Link>
+            </Typography>
+          </Box>
+          <Button id='loginbutton' role='login' variant="contained" color="success" onClick={login} sx={{ marginTop: '30px',marginLeft:'210px' }}>login</Button>
+        </div>
+
+      </div>
+
+    </>
   )
 }
 
--- frontend/src/root/SignUp.jsx
+++ frontend/src/root/SignUp.jsx
@@ -8,7 +8,6 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
-import { styled } from '@mui/system';
 import NavigationBtn from '../components/NavigationBtn';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -18,9 +17,8 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
-const Registerdiv = styled('div')({
-  padding: '10px'
-})
+
+
 
 // signup page
 function SignUp() {
@@ -30,15 +28,20 @@ function SignUp() {
   const [name, setName] = React.useState('');
   const [role, setRole] = React.useState(0);
   const [authvalur, setAuthvalue] = React.useState(0)
-
   const [passport, setPassport] = React.useState('');
   const [medicalCard, setMedicalCard] = React.useState('');
   const [driverLicense, setDriverLicense] = React.useState('')
+  const [code, setCode] = React.useState('')
+  const [codeinput, setCodeinput] = React.useState('')
 
 
   const [isChecked1, setChecked1] = React.useState(false);
   const [isChecked2, setChecked2] = React.useState(false);
   const [isChecked3, setChecked3] = React.useState(false);
+
+
+  const [isButtonDisabled, setIsButtonDisabled] = React.useState(false);
+  const [countdown, setCountdown] = React.useState(60);
 
   useEffect(() => {
     if (isChecked1 === true || isChecked2 === true || isChecked3 === true) {
@@ -46,17 +49,27 @@ function SignUp() {
     } else {
       setAuthvalue(0)
     }
-
   }, [isChecked1, isChecked2, isChecked3]);
 
+  useEffect(() => {
+    if (countdown > 0 && isButtonDisabled) {
+      const timer = setTimeout(() => {
+        setCountdown(prevCountdown => prevCountdown - 1);
+      }, 1000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [countdown, isButtonDisabled]);
 
 
 
   const handleChange = (event) => {
     setRole(event.target.value)
   };
-  const navigate = useNavigate();
 
+  const navigate = useNavigate();
   const [open, setOpen] = React.useState(false);
   const handleClose = () => {
     setOpen(false);
@@ -64,19 +77,49 @@ function SignUp() {
   const nav = () => {
     navigate('/login');
   }
+
+  function recievecode() {
+    const res = apiCall('/sendcode', 'POST', { 'email': email })
+    res.then((data) => {
+      if (data.error) {
+        alert(data.error)
+      } else {
+        setCode(data.code)
+
+      }
+    })
+    setIsButtonDisabled(true);
+    setCountdown(60);
+
+    const timer = setInterval(() => {
+      setCountdown(prevCountdown => prevCountdown - 1);
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(timer);
+      setIsButtonDisabled(false);
+    }, 60000);
+
+
+  }
+
   // register function
   async function register() {
     if (checkEmail(email) && checkPassword(password1) && verifyPassword(password1, password2) && checkName(name)) {
-      const res = apiCall('/register', 'POST', 
-      { 'email': email, 'password': password1, 'role': role, 'name':name, 'passport': passport, 'medicalCard': medicalCard, 'driverLicense':driverLicense});
-      
-      res.then((data) => {
-        if (data.error) {
-          alert(data.error);
-        } else {
-          setOpen(true);
-        }
-      })
+      if (code == codeinput) {
+        const res = apiCall('/register', 'POST',
+          { 'email': email, 'password': password1, 'role': role, 'name': name, 'passport': passport, 'medicalCard': medicalCard, 'driverLicense': driverLicense });
+        res.then((data) => {
+          if (data.error) {
+            alert(data.error);
+          } else {
+            setOpen(true);
+          }
+        })
+      } else {
+        alert('invalid code');
+      }
+
     }
   }
 
@@ -84,9 +127,10 @@ function SignUp() {
     <>
       <NavigationBtn></NavigationBtn>
 
-      <div style={{ background: '#E2E6E6', width: '1540px', height: '10000px' }}>
+      <div style={{ background: '#E2E6E6', width: '1540px' }}>
         <div style={{ width: '550px', height: '700px', background: 'white', margin: 'auto', marginTop: '50px' }}>
-          <div style={{ position: 'relative', top: '10px', left: '220px', marginBottom: '70px', fontSize: '30px' }}>Sign up</div>
+          <div style={{ position: 'relative', top: '10px', left: '220px', marginBottom: '30px', fontSize: '30px' }}>Sign up</div>
+            
           <Box
             component="form"
             sx={{
@@ -101,24 +145,24 @@ function SignUp() {
                 setEmail(e.target.value);
               }} />
             <br></br>
-            <TextField id="filled-basic" label="Password" variant="filled" style={{ width: '400px' }}
+            <TextField id="filled-basic" label="Password" variant="filled" style={{ width: '400px', marginLeft: '60px' }}
               onChange={(e) => {
                 setPassword1(e.target.value);
               }} />
             <br></br>
-            <TextField id="filled-basic" label="Verify Password" variant="filled" style={{ width: '400px' }}
+            <TextField id="filled-basic" label="Verify Password" variant="filled" style={{ width: '400px', marginLeft: '60px' }}
               onChange={(e) => {
                 setPassword2(e.target.value);
               }} />
             <br></br>
-            <TextField id="filled-basic" label="name" variant="filled" style={{ width: '400px' }}
+            <TextField id="filled-basic" label="name" variant="filled" style={{ width: '400px', marginLeft: '60px' }}
               onChange={(e) => {
                 setName(e.target.value);
               }} />
           </Box>
-          
-          
-          <Box sx={{ minWidth: 120 }}>
+
+
+          <Box sx={{ minWidth: 160, marginLeft: '60px' }}>
             <FormControl fullWidth>
               <InputLabel id="demo-simple-select-label">role</InputLabel>
               <Select
@@ -134,10 +178,10 @@ function SignUp() {
               </Select>
             </FormControl>
           </Box>
-          
-          
+
+
           <FormGroup>
-            <div style={{ display: 'flex' }}>
+            <div style={{ display: 'flex', marginLeft: '60px', marginTop: '10px' }}>
               <FormControlLabel control={<Checkbox />} label="Passport" onChange={(e) => setChecked1(e.target.checked)} />
               <div>
                 <TextField
@@ -147,13 +191,12 @@ function SignUp() {
                   size="small"
                   onChange={(e) => {
                     setPassport(e.target.value)
-                   
-                  }} 
-                />
 
+                  }}
+                />
               </div>
             </div>
-            <div style={{ display: 'flex' }}>
+            <div style={{ display: 'flex', marginLeft: '60px', marginTop: '10px' }}>
               <FormControlLabel control={<Checkbox />} label="Driver License" onChange={(e) => setChecked2(e.target.checked)} />
               <div>
                 <TextField
@@ -163,14 +206,14 @@ function SignUp() {
                   size="small"
                   onChange={(e) => {
                     setDriverLicense(e.target.value)
-                   
-                  }} 
+
+                  }}
                 />
 
               </div>
             </div>
 
-            <div style={{ display: 'flex' }}>
+            <div style={{ display: 'flex', marginLeft: '60px', marginTop: '10px' }}>
               <FormControlLabel control={<Checkbox />} label="Medical Card" onChange={(e) => setChecked3(e.target.checked)} />
               <div>
                 <TextField
@@ -180,26 +223,27 @@ function SignUp() {
                   size="small"
                   onChange={(e) => {
                     setMedicalCard(e.target.value)
-                  }} 
+                  }}
                 />
 
               </div>
             </div>
           </FormGroup>
-          {authvalur === 1 && <div>
+          {authvalur === 1 && <div style={{ marginLeft: '60px', marginTop: '10px' }}>
             <input
-              value={email}
-              // onChange={(e) => setEmail(e.target.value)}
-              placeholder="Enter your email"
+              placeholder="Enter Verification Code"
+              onChange={(e) => {
+                setCodeinput(e.target.value)
+              }}
             />
-            <button>
-              Send Verification Code
+            <button onClick={recievecode} disabled={isButtonDisabled}>
+              {isButtonDisabled ? `Resend in ${countdown} seconds` : 'Send Verification Code'}
             </button>
 
           </div>}
 
 
-          {authvalur === 1 && <Button id='registerbutton' role="register" variant="contained" color="success" onClick={register} sx={{ marginTop: '30px' }}>register</Button>}
+          {authvalur === 1 && <Button id='registerbutton' role="register" variant="contained" color="success" onClick={register} sx={{ marginTop: '30px', marginLeft: '210px' }}>register</Button>}
 
 
 
