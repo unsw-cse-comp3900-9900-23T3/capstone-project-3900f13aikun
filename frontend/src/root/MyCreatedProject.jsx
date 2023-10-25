5594ea864ca3620ac007aac38998c6f150ca9d34
Jackson Yao <91328227+yaojiandashuaige@users.noreply.github.com>
2023-10-25 19:57:24 +1100

    cccc

--- frontend/src/root/MyCreatedProject.jsx
+++ frontend/src/root/MyCreatedProject.jsx
@@ -17,50 +17,125 @@ import CardContent from "@mui/material/CardContent";
 import { useNavigate } from "react-router-dom";
 import NavigationBtn from "../components/NavigationBtn";
 import { apiCall } from "../components/HelpFunctions";
+import { Table, TableCell, TableRow, TableHead, TableBody, TableContainer, Paper } from "@mui/material";
 
 export default function MyCreatedProject() {
     const navigate = useNavigate();
 
     const [projects, setProjects] = React.useState([]);
+    const [joinStatus, setJoinStatus] = React.useState({});
 
     React.useEffect(() => {
+        // 假设apiCall是获取项目列表的函数
         apiCall(`/project/${localStorage.getItem("userId")}`, "GET").then((res) => {
             setProjects(res);
         });
     }, []);
 
+    const handleEdit = () => {
+
+    }
+
+    const myProjects = projects.filter((item) => item.creatorId === item.userId);
+    const otherProjects = projects.filter((item) => item.creatorId !== item.userId);
+
+
+    const handleJoin = (projectId) => {
+        // 切换加入/离开状态
+        setJoinStatus((prevJoinStatus) => ({
+            ...prevJoinStatus,
+            [projectId]: !prevJoinStatus[projectId],
+        }));
+
+        // 处理加入/离开项目的逻辑
+        console.log(joinStatus[projectId] ? `Leave project with ID ${projectId}` : `Join project with ID ${projectId}`);
+    };
+
+    const handleLeave = (projectId) => {
+        // 处理离开项目的逻辑
+        console.log(`Leave project with ID ${projectId}`);
+        // 从列表中移除项目
+        setProjects((prevProjects) => prevProjects.filter((item) => item.id !== projectId));
+    };
+
     return (
         <>
             <NavigationBtn></NavigationBtn>
-            <Box sx={{ py: 2 }}>
-                <Typography variant="h2" gutterBottom>
-                    My Created Projects
-                </Typography>
-                <Button
-                    variant="contained"
-                    color="success"
-                    onClick={() => {
-                        navigate("/create-project");
-                    }}>
-                    New Project
-                </Button>
-                <Box sx={{ mt: 3 }}>
-                    {projects.map((item) => (
-                        <Card key={item.id}>
-                            <CardContent>
-                                <Typography variant="h4" gutterBottom>
-                                    Project {item.id}
-                                </Typography>
-                                <Typography color="text.secondary">location: {item.location}</Typography>
-                                <Typography color="text.secondary">Job Classification: {item.job_classification}</Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small">Edit</Button>
-                            </CardActions>
-                        </Card>
-                    ))}
-                </Box>
-            </Box>
+            <Typography variant="h6" component="div" style={{marginRight: '900px'}}>
+                My Group
+            </Typography>
+            <br></br>
+            <TableContainer component={Paper} style={{ maxWidth: 1000, margin: '0 auto'}}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Project ID</TableCell>
+                            <TableCell>Location</TableCell>
+                            <TableCell>Job Classification</TableCell>
+                            <TableCell>Action</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {myProjects.map((item) => (
+                            <TableRow key={item.id}>
+                                <TableCell>Project {item.id}</TableCell>
+                                <TableCell>{item.location}</TableCell>
+                                <TableCell>{item.job_classification}</TableCell>
+                                <TableCell>
+                                    <Button onClick={() => handleLeave(item.id)}>
+                                        Leave
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+
+            <Typography variant="h6" component="div" style={{ marginTop: '20px' }}>
+                Other Groups
+            </Typography>
+            <TableContainer component={Paper} style={{ maxWidth: 1000, margin: '0 auto' }}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Project ID</TableCell>
+                            <TableCell>Location</TableCell>
+                            <TableCell>Job Classification</TableCell>
+                            <TableCell>Action</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {otherProjects.map((item) => (
+                            <TableRow key={item.id}>
+                                <TableCell>Project {item.id}</TableCell>
+                                <TableCell>{item.location}</TableCell>
+                                <TableCell>{item.job_classification}</TableCell>
+                                <TableCell>
+                                    {joinStatus[item.id] ? (
+                                        <Button onClick={() => handleLeave(item.id)}>
+                                            Leave
+                                        </Button>
+                                    ) : (
+                                        <Button onClick={() => handleJoin(item.id)}>
+                                            Join
+                                        </Button>
+                                    )}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Button
+                variant="contained"
+                color="success"
+                onClick={() => {
+                    navigate("/create-project");
+                }}>
+                New Project
+            </Button>
         </>
     );
 }
