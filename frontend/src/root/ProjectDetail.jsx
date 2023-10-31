6584b2fc61d6a21b9c1efe2672554fa173d6cfc5
Jackson Yao <91328227+yaojiandashuaige@users.noreply.github.com>
2023-10-31 23:02:15 +1100

    update project detial

--- frontend/src/root/ProjectDetail.jsx
+++ frontend/src/root/ProjectDetail.jsx
@@ -6,6 +6,7 @@ import NavigationBtn from "../components/NavigationBtn";
 import { useNavigate, useParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { apiCall } from "../components/HelpFunctions";
+import { getJobType, getPaymentType, getOpportunityType } from "../components/EnumMap";
 
 function ProjectDetail() {
   const { id } = useParams();
@@ -24,27 +25,59 @@ function ProjectDetail() {
   return (
     <>
       <NavigationBtn></NavigationBtn>
-      <Box sx={{ paddingX: 10, paddingY: 5 }}>
-        <Typography variant="h2" gutterBottom>
+      <Box sx={{ paddingX: 10, paddingY: 5, marginRight: '200px' }}>
+        <Typography variant="h3" gutterBottom>
           {projectInfo.title}
         </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          {projectInfo.problem_statement}
+        <Typography variant="body1" gutterBottom>
+          Location: <span style={{ color: '#555' }}>{projectInfo.location}</span>
         </Typography>
-        <Typography variant="body2" gutterBottom>
-          Location: {projectInfo.location}
+        <Typography variant="body1" gutterBottom>
+          Job classification: <span style={{ color: '#555' }}>{getJobType(projectInfo.job_classification)}</span>
         </Typography>
-        <Typography variant="body2" gutterBottom>
-          Opportunit Type: {projectInfo.opportunity_type}
+        <Typography variant="body1" gutterBottom>
+          {getOpportunityType(projectInfo.opportunity_type)} | {getPaymentType(projectInfo.payment_type)}
         </Typography>
-        <Typography variant="body2" gutterBottom>
-          Availability requirement: {projectInfo.requirement}
+        <Typography variant="body1" gutterBottom>
+          <span style={{ color: 'gray' }}>Posted 12h ago</span>
         </Typography>
-
-        <Box sx={{ display: "flex", gap: 10, my: 5 }}>
+      
+        <Box sx={{ display: "flex", gap: 8, my: 2 }}>
           <Button variant="contained">Apply</Button>
           <Button variant="outlined">Save</Button>
         </Box>
+        
+        <br></br>
+        <Typography variant="h6" gutterBottom>
+          Problem statement
+          <br></br>
+          <span style={{ color: '#555', fontSize: '16px' }}>{projectInfo.problem_statement}</span>
+        </Typography>
+        <br></br>
+        <Typography variant="h6" gutterBottom>
+          Avalability Requirement
+          <br></br>
+          <span style={{ color: '#555', fontSize: '16px' }}>{projectInfo.requirement}</span>
+        </Typography>
+        <br></br>
+        <Typography variant="h6" gutterBottom>
+          Desired outcomes
+          <br></br>
+          <span style={{ color: '#555', fontSize: '16px' }}>{projectInfo.desired_outcomes}</span>
+        </Typography>
+        <br></br>
+        <Typography variant="h6" gutterBottom>
+          Potential deliverable
+          <br></br>
+          <span style={{ color: '#555', fontSize: '16px' }}>{projectInfo.potential_deliverable}</span>
+        </Typography>
+        <br></br>
+        <Typography variant="h6" gutterBottom>
+          Expected delivery cycle
+          <br></br>
+          <span style={{ color: '#555', fontSize: '16px' }}>{projectInfo.expected_delivery_cycle}</span>
+        </Typography>
+
       </Box>
     </>
   );
