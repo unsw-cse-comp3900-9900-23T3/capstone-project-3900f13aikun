dde7c53421eddeaf05e3b6f5f9fb88ce9ab01ff7
Mike0325 <85435378+Mike0325-c@users.noreply.github.com>
2023-11-10 14:12:38 +0800

    change

--- frontend/src/root/InitialDash.jsx
+++ frontend/src/root/InitialDash.jsx
@@ -3,21 +3,20 @@ import NavigationBtn from "../components/NavigationBtn";
 import { useNavigate, useParams } from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import { apiCall } from "../components/HelpFunctions";
-import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import { getJobType, getPaymentType, getOpportunityType } from "../components/EnumMap";
+import { getJobType, getPaymentType, getOpportunityType, getIntention } from "../components/EnumMap";
 import Box from "@mui/material/Box";
 import { Dashbackground, Dashtextfield } from "../components/StyledElement";
-import Stack from '@mui/material/Stack';
+
+
 
 const InitialDash = () => {
   const navigate = useNavigate();
@@ -32,6 +31,8 @@ const InitialDash = () => {
   const [role, setRole] = React.useState(0);
   const [recProjects, setRecProjects] = React.useState([]);
   const [savedProjects, setSavedProjects] = React.useState([]);
+  const [currentPage, setCurrentPage] = React.useState('');
+  const [recSupervisor, setRecSupervsior] = React.useState([]);
 
   React.useEffect(() => {
     if (localStorage.getItem("token")) {
@@ -58,6 +59,13 @@ const InitialDash = () => {
     });
   };
 
+
+  const getRecSupervisors = () => {
+    apiCall(`/recommend/teacher`, "GET").then((res) => {
+      setRecSupervsior(res);
+    });
+  }
+
   function handleApply() {
     apiCall().then((res) => {
       navigate("/Application");
@@ -66,17 +74,25 @@ const InitialDash = () => {
 
   const handleSave = (id) => {
     apiCall(`/saved/project/${id}`, "GET").then((res) => {
-      handleSearch();
+      handleSearch("page-search");
     });
   };
 
   const handleUnSave = (id) => {
     apiCall(`/unsaved/project/${id}`, "GET").then((res) => {
-      handleSearch();
+      handleSearch("page-search");
     });
   };
 
-  const handleSearch = () => {
+  const getProjectDetail= (id) => {
+    if (localStorage.getItem("token")) {
+      navigate(`/project-detail/${id}`)
+    } else {
+      alert('Please logged in');
+    }
+  }
+
+  const handleSearch = (page) => {
     const data = { keyword, location, job_classification: classification, opportunity_type: opportunityType, publish_date_type: publishTime, payment_type: paymentType };
     let temp = [];
     Object.keys(data).forEach((key) => {
@@ -85,20 +101,20 @@ const InitialDash = () => {
       }
     });
     const qs = temp.join("&");
-
     apiCall(`/project?${qs}`, "GET").then((res) => {
       setProjectList(res);
     });
+    setCurrentPage(page)
   };
 
   useEffect(() => {
     getRecProjects();
+    getRecSupervisors();
   }, []);
 
   useEffect(() => {
     handleSearch();
   }, []);
-
   return (
     <div>
       <NavigationBtn />
@@ -218,7 +234,7 @@ const InitialDash = () => {
       </Dashbackground>
 
       {/* search results */}
-<Box sx={{ pt: 3, display: "flex", flexDirection: "column", alignItems: "center", gap: 5 }}>
+      <Box sx={{ pt: 3, display: "flex", flexDirection: "column", alignItems: "center", gap: 5 }}>
         <Typography>the total numbers of projects: {projectList.length}</Typography>
         {projectList.map((item) => (
           <Card key={item.id} sx={{ maxWidth: 600, minWidth: 400, border: "2px solid lightgray" }}>
@@ -269,80 +285,132 @@ const InitialDash = () => {
       </Box>
 
       {/* recommand system */}
-      <Box sx={{ pt: 3, gap: 3, display: "flex", justifyContent: "center" }}>
-        <Box sx={{ pt: 3, gap: 3, display: "flex", flexDirection: "column" }}>
-          <Typography variant="h5" gutterBottom>
-            Recommand Projects
-          </Typography>
-          {localStorage.getItem("token") ? (recProjects.slice(0, 3).map((item) => (
-            <Card key={item.id} sx={{ maxWidth: 400, minWidth: 300, border: "2px solid lightgray", borderRadius: "30px" }}>
-              <CardContent sx={{ marginLeft: "10px" }}>
-                <Typography
-                  sx={{ textDecorationLine: "underline", cursor: "pointer" }}
-                  gutterBottom
-                  variant="h5"
-                  component="div"
-                  onClick={() => {
-                    navigate(`/project-detail/${item.id}`);
-                  }}>
-                  {item.title}
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                  Location: <span style={{ color: "#555" }}>{item.location}</span>
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                  Project type: <span style={{ color: "#555" }}>{getJobType(item.job_classification)}</span>
-                </Typography>
-              </CardContent>
-            </Card>
-          ))) : (
-            <span style={{ color: "gray" }}>Not logged in, please log in to view content.</span>
-          )}
-          {localStorage.getItem("token") ? (
-            <Button
-              sx={{ width: "180px", height: "50px", border: "1px solid #1E90FF", borderRadius: "90px" }}
-              onClick={() => { navigate('/recommend-projects'); }}>
-              View All ({recProjects.length})
-            </Button>
+      {currentPage !== 'page-search' && (
+        <Box sx={{ pt: 3, gap: 3, display: "flex", justifyContent: "center" }}>
+          <Box sx={{ pt: 3, gap: 3, display: "flex", flexDirection: "column" }}>
+            <Typography variant="h5" gutterBottom>
+              Recommended Projects
+            </Typography>
+            {(localStorage.getItem("token") && recProjects.length !== 0) ? (recProjects.slice(0, 3).map((item) => (
+              <Card key={item.id} sx={{ maxWidth: 350, minWidth: 320, border: "2px solid lightgray", borderRadius: "30px" }}>
+                <CardContent sx={{ marginLeft: "10px" }}>
+                  <Typography
+                    sx={{ textDecorationLine: "underline", cursor: "pointer" }}
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    onClick={() => {
+                      navigate(`/project-detail/${item.id}`);
+                    }}>
+                    {item.title}
+                  </Typography>
+                  <Typography variant="body1" gutterBottom>
+                    Location: <span style={{ color: "#555" }}>{item.location}</span>
+                  </Typography>
+                  <Typography variant="body1" gutterBottom>
+                    Project type: <span style={{ color: "#555" }}>{getJobType(item.job_classification)}</span>
+                  </Typography>
+                </CardContent>
+              </Card>
+            ))) : (
+              <span style={{ color: "gray", width: "400px", fontSize: "20px" }}>Please logged in or there is no project for you.</span>
+            )}
+            {(localStorage.getItem("token") && recProjects.length !== 0) ? (
+              <Button
+                sx={{ width: "180px", height: "50px", border: "1px solid #1E90FF", borderRadius: "90px" }}
+                onClick={() => { navigate('/recommend-projects'); }}>
+                View All ({recProjects.length})
+              </Button>
+            ) : null}
+          </Box>
+
+          {/* Saved Projects */}
+          <Box sx={{ pt: 3, display: "flex", flexDirection: "column", gap: 3, marginLeft: "100px" }}>
+            <Typography variant="h5" gutterBottom>
+              Saved Projects
+            </Typography>
+            {(localStorage.getItem("token") && savedProjects.length !== 0) ? (savedProjects.slice(0, 3).map((item) => (
+              <Card sx={{ maxWidth: 350, minWidth: 320, border: '2px solid lightgray', borderRadius: "30px" }}>
+                <CardContent sx={{ marginLeft: "10px" }}>
+                  <Typography
+                    sx={{ textDecorationLine: "underline", cursor: "pointer" }}
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    onClick={() => {
+                      navigate(`/project-detail/${item.id}`);
+                    }}>
+                    {item.title}
+                  </Typography>
+                  <Typography variant="body1" gutterBottom>
+                    Location: <span style={{ color: '#555' }}>{item.location}</span>
+                  </Typography>
+                  <Typography variant="body1" gutterBottom>
+                    Project type: <span style={{ color: '#555' }}>{getJobType(item.job_classification)}</span>
+                  </Typography>
+                </CardContent>
+              </Card>
+            ))) : (
+              <span style={{ color: "gray", width: "400px", fontSize: "20px" }}>
+                Please logged in or use the save button on each job listing to to save it.
+              </span>
+            )}
+            {(localStorage.getItem("token") && savedProjects.length !== 0) ? (
+              <Button
+                sx={{ width: "180px", height: "50px", border: "1px solid #1E90FF", borderRadius: "90px" }}
+                onClick={() => { navigate('/saved-projects'); }}>
+                View All ({savedProjects.length})
+              </Button>
+            ) : null}
+          </Box>
+
+          {/* Saved Academic Supervisors*/}
+          {role !== 3 ? (
+            <Box sx={{ pt: 3, display: "flex", flexDirection: "column", gap: 3, marginLeft: "100px" }}>
+              <Typography variant="h5" gutterBottom>
+                Recommended Academic Supervisors
+              </Typography>
+              {(localStorage.getItem("token") && recSupervisor.length !== 0) ? (recSupervisor.slice(0, 3).map((item) => (
+                <Card key={item.user_id} sx={{ maxWidth: 350, minWidth: 320, border: '2px solid lightgray', borderRadius: "30px" }}>
+                  <CardContent sx={{ marginLeft: "10px" }}>
+                    <Typography
+                      sx={{ textDecorationLine: "underline", cursor: "pointer" }}
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                      onClick={() => {
+                        navigate(`/profile-detail/${item.user_id}`);
+                      }}>
+                      {item.name}
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                      Email: <span style={{ color: '#555' }}>{item.email}</span>
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                      Project Intention: <span style={{ color: '#555' }}>
+                        {Array.isArray(item.project_intention) ? item.project_intention.map(getIntention).join(', ') : ''}
+                      </span>
+                    </Typography>
+                  </CardContent>
+                </Card>
+              ))) : (
+                <span style={{ color: "gray", width: "400px", fontSize: "20px" }}>
+                  Please logged in or there is no academic supervisior for you.
+                </span>
+              )}
+              {(localStorage.getItem("token") && recSupervisor.length !== 0) ? (
+                <Button
+                  sx={{ width: "180px", height: "50px", border: "1px solid #1E90FF", borderRadius: "90px" }}
+                  onClick={() => { navigate('/recommend-academic-supervisors'); }}>
+                  View All ({recSupervisor.length})
+                </Button>
+              ) : null}
+            </Box>
           ) : null}
         </Box>
-        <Box sx={{ pt: 3, display: "flex", flexDirection: "column", gap: 3, marginLeft: "200px" }}>
-          <Typography variant="h5" gutterBottom>
-            Saved Projects
-          </Typography>
-          {localStorage.getItem("token") ? (savedProjects.slice(0, 3).map((item) => (
-            <Card sx={{ maxWidth: 400, minWidth: 300, border: '2px solid lightgray', borderRadius: "30px" }}>
-              <CardContent sx={{ marginLeft: "10px" }}>
-                <Typography
-                  sx={{ textDecorationLine: "underline", cursor: "pointer" }}
-                  gutterBottom
-                  variant="h5"
-                  component="div"
-                  onClick={() => {
-                    navigate(`/project-detail/${item.id}`);
-                  }}>
-                  {item.title}
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                  Location: <span style={{ color: '#555' }}>{item.location}</span>
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                  Project type: <span style={{ color: '#555' }}>{getJobType(item.job_classification)}</span>
-                </Typography>
-              </CardContent>
-            </Card>
-          ))) : (
-            <span style={{ color: "gray" }}>Not logged in, please log in to view content.</span>
-          )}
-          {localStorage.getItem("token") ? (
-            <Button
-              sx={{ width: "180px", height: "50px", border: "1px solid #1E90FF", borderRadius: "90px" }}
-              onClick={() => { navigate('/saved-projects'); }}>
-              View All ({savedProjects.length})
-            </Button>
-          ) : null}
-        </Box>
-      </Box>
+      )}
+
+
     </div>
   );
 };
